7097bee82586745c4d054a93421f0ee4d394c674
numtel <ben@latenightsketches.com>
2022-09-07 14:27:01 -0700

    Multiple collateral and investor test case

--- test/Lwned.test.js
+++ test/Lwned.test.js
@@ -178,4 +178,60 @@ exports.loanDefaulted = async function({
   assert.strictEqual(Number(await token.methods.balanceOf(accounts[0]).call()), toGive);
 
 }
-// TODO Test with multiple collateral tokens, investors of different amounts
+
+exports.multipleCollateralAndInvestor = async function({
+  web3, accounts, deployContract, loadContract, throws, BURN_ACCOUNT, increaseTime,
+}) {
+  const factory = await deployContract(accounts[0], 'Lwned');
+  const token = await deployContract(accounts[0], 'MockERC20');
+  const token2 = await deployContract(accounts[0], 'MockERC20');
+
+  const now = (await web3.eth.getBlock('latest')).timestamp;
+  const deadlineIssue = 30;
+  const deadlineRepay = 300;
+  const toGive = 1000000;
+  const toRepay  = 1090000;
+  const interest = toRepay - toGive;
+  const collateral = 500000;
+  const collateral2 = 800000;
+  const submissionStatement = "heyo gimme some skrilla";
+
+  await token.sendFrom(accounts[0]).mint(accounts[0], collateral);
+  await token.sendFrom(accounts[0]).approve(factory.options.address, collateral);
+  await token2.sendFrom(accounts[0]).mint(accounts[0], collateral2);
+  await token2.sendFrom(accounts[0]).approve(factory.options.address, collateral2);
+
+  // Create loan
+  const result = await factory.sendFrom(accounts[0]).newApplication(
+    token.options.address,
+    toGive,
+    toRepay,
+    now + deadlineIssue,
+    now + deadlineIssue + deadlineRepay,
+    [token.options.address, token2.options.address],
+    [collateral, collateral2],
+    submissionStatement
+  );
+  const loan = await loadContract('Loan', result.events.NewApplication.returnValues.loan);
+
+  // Invest and divest from the loan
+  const give1 = 0.25, give2 = 1 - give1;
+  await token.sendFrom(accounts[1]).mint(accounts[1], toGive * give1);
+  await token.sendFrom(accounts[1]).approve(loan.options.address, toGive * give1);
+  await loan.sendFrom(accounts[1]).invest(toGive * give1);
+  await token.sendFrom(accounts[2]).mint(accounts[2], toGive * give2);
+  await token.sendFrom(accounts[2]).approve(loan.options.address, toGive * give2);
+  await loan.sendFrom(accounts[2]).invest(toGive * give2);
+
+  await loan.sendFrom(accounts[0]).loanIssue();
+  await increaseTime(deadlineIssue + deadlineRepay + 5);
+
+  // Can divest after default
+  await loan.sendFrom(accounts[1]).divest(toGive * give1);
+  assert.strictEqual(Number(await token.methods.balanceOf(accounts[1]).call()), collateral * give1);
+  assert.strictEqual(Number(await token2.methods.balanceOf(accounts[1]).call()), collateral2 * give1);
+  await loan.sendFrom(accounts[2]).divest(toGive * give2);
+  assert.strictEqual(Number(await token.methods.balanceOf(accounts[2]).call()), collateral * give2);
+  assert.strictEqual(Number(await token2.methods.balanceOf(accounts[2]).call()), collateral2 * give2);
+
+}
